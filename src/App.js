33efde488f688a8e1b10f1cd9a131617b6c475f5
Ewa Olczak <ewa.olczak@gmail.com>
2022-12-31 21:25:06 +0100

    Add BookList and BookForm components (with display error)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import BookForm from './components/BookForm/BookForm';
+import BooksList from './components/BooksList/BooksList';
+import useState from 'react';
+
 const App = () => {
+  
+  // eslint-disable-next-line no-unused-vars
+  const [books, setBooks] = useState([
+    { id: 1, title: 'of Mice and Men', author: 'John Steinbeck' },
+    { id: 2, title: 'the Witcher', author: 'Andrzej Sapkowski' }
+  ]);
+
   return (
     <div>
       <h1>Book React App</h1>
+      <BooksList books={books} />
+      <BookForm />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/BooksList/BooksList.js
@@ -0,0 +1,13 @@
+const BooksList = ({ books }) => {
+  return (
+    <ul>
+      {books.map(book => 
+        <li key={book.id}>
+          {book.title} by {book.author}
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default BooksList;
